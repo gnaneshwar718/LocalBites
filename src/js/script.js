b1c047431f40de95238eceec3a78a8cb349430bb
gnaneshwar <gnaneshwar718@gmail.com>
2025-12-22 15:50:23 +0530

    chore: resolve merge conflicts after stash apply

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
-// test
-'use strict';
+// // test
+// 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('LocalBites app loaded');
-});
-console.log('test');
-// lint clean test
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('LocalBites app loaded');
+// });
+// console.log('test');
+// // lint clean test
