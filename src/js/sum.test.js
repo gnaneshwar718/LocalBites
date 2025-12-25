f2c13b7bee14167c9688ac2bfc702dfbd269acc5
gnaneshwar <gnaneshwar718@gmail.com>
2025-12-25 22:10:04 +0530

    feat(auth): create sign-in and sign-up pages

--- src/js/sum.test.js
+++ src/js/sum.test.js
@@ -1,14 +1,15 @@
- const sum = require('./sum');
+// 'use strict'; 
+// const sum = require('./sum');
 
-/** test("properly adds two numbers", () => {
-    if(sum (1, 2) === 3){
-        console.log("executes code");
-    }
-    else {
-        throw error;
-    }
-}) **/
+// /** test("properly adds two numbers", () => {
+//     if(sum (1, 2) === 3){
+//         console.log("executes code");
+//     }
+//     else {
+//         throw error;
+//     }
+// }) **/
 
-test("properly adds two numbers", () => {
-    expect(sum(1, 2)).toBe(3);
-}) 
+// test("properly adds two numbers", () => {
+//     expect(sum(1, 2)).toBe(3);
+// }) 
