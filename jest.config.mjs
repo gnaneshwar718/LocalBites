40ad2553d695378be352fa095076fedad294dd6d
Rakshitha <rakshithabanapur29@gmail.com>
2025-12-30 20:44:37 +0530

    fix: fixed test failure in pr

---
+++ jest.config.mjs
@@ -0,0 +1,5 @@
+export default {
+    testEnvironment: "jsdom",
+    testMatch: ["**/tests/**/*.test.mjs", "**/?(*.)+(spec|test).[tj]s?(x)"],
+    moduleFileExtensions: ["js", "mjs", "json", "jsx", "ts", "tsx", "node"],
+};
