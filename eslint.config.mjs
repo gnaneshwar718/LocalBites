287ec670f9f1a6d538794e9aea4d2138ebdb330d
gnaneshwar <gnaneshwar718@gmail.com>
2026-01-13 09:10:13 +0530

    fix: adding communint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -51,7 +51,7 @@ export default [
     files: ['server.js', 'commitlint.config.js'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'script',
+      sourceType: 'module',
       globals: {
         ...globals.node,
       },
